ce07f01beb79ebf14c4a8ec3dbe1501d09477b99
ImoYs <112687423+ImoYs@users.noreply.github.com>
2024-02-28 19:09:15 +0700

    add states

--- Kitchen_status/index.php
+++ Kitchen_status/index.php
@@ -16,9 +16,11 @@
 	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
 	<link rel="stylesheet">
 	<script src="https://kit.fontawesome.com/c1134aa968.js" crossorigin="anonymous"></script>
-
+	<link rel="stylesheet" href=".css">
 	<link rel="stylesheet" href="styles.css">
 	<link rel="stylesheet" href="button.css">
+	<link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
 	<!-- <link href="styles.css" rel="stylesheet" /> -->
 	<title>Document</title>
 </head>
@@ -27,7 +29,23 @@ session_start();
 include('../connectDatabase/connectToDatabase.php');
 
 $conn = new database();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// ตรวจสอบว่ามีค่าที่ถูกส่งมากับชื่อ order_id และ order_status หรือไม่
+	if (isset($_POST['order_id']) && isset($_POST['order_status'])) {
+		// รับค่า order_id และ order_status จากการส่งคำขอ HTTP POST
+		$orderID = $_POST['order_id'];
+		$orderStatus = $_POST['order_status'];
 
+		if($orderStatus == 'take'){
+			$query = "UPDATE OrderTable SET orderStatus = '$orderStatus'  WHERE orderID = $orderID";
+			mysqli_query($conn->getDatabase(), $query);
+		}
+		else{
+			$query = "UPDATE OrderTable SET orderStatus = '$orderStatus'  WHERE orderID = $orderID";
+			mysqli_query($conn->getDatabase(), $query);
+		}
+	}
+}
 
 ?>
 
@@ -59,10 +77,15 @@ $conn = new database();
 
 
 	<?php
-	$sqlOrder = "SELECT * FROM OrderTable;";
+	$sqlOrder = "SELECT * FROM OrderTable WHERE orderStatus = 'take' OR orderStatus = 'doing';";
 	$result = mysqli_query($conn->getDatabase(), $sqlOrder);
 
-	$num = mysqli_num_rows($result);
+	if ($result) {
+		$num = mysqli_num_rows($result);
+	} else {
+		echo "การดึงข้อมูลผิดพลาด: " . mysqli_error($conn->getDatabase());
+	}
+
 	echo
 	'<div class="">
 				<div style="height: auto; background-color: white;">
@@ -74,8 +97,6 @@ $conn = new database();
 				</div>
 			</div>';
 	?>
-
-
 
 	<div class="container">
 		<div class="row">
@@ -89,53 +110,60 @@ $conn = new database();
 					if ($row['orderStatus'] == "take" || $row['orderStatus'] == "doing") {
 						echo
 						'<div class="col-sm-4 py-2">
-							<div class="card">
+							<div class="card" style="width: 370px;">
 								<div class="card-bg">
 									<h3 class="card-title text-center" style="line-height: 80px">โต็ะ หมายเลข ' . $row['tableid'] . '</h3>
 								</div>
 									<div class="card-body">
-											<h5 class="card-subtitle mb-3">รายการอาหารที่สั่ง</h5>
+											<div class="row">
+												<div class="col-6">รายการอาหารที่สั่ง</div>';
+												if ($row['orderStatus'] == "take"){
+													echo '<div class="col-6"><button type="button" class="btn btn-danger">รอดำเนิดการ</button></div>';
+												} else{
+													echo '<div class="col-6"><button type="button" class="btn btn-warning">รอดำเนิดการ</button></div>';
+												}
+										echo'</div><br>
 									<div class="row">
 										<div class="col-8">รายการอาหาร</div>
 										<div class="col-4">จำนวน</div>
 									</div><br>
 									<div class="menu">';
-								$orderid = $row['orderID'];
-								$select_sql = "SELECT orderMenu FROM OrderTable WHERE orderid = '$orderid'";
-								$resultorder = mysqli_query($conn->getDatabase(), $select_sql);
+						$orderid = $row['orderID'];
+						$select_sql = "SELECT orderMenu FROM OrderTable WHERE orderid = '$orderid'";
+						$resultorder = mysqli_query($conn->getDatabase(), $select_sql);
 
-								if ($resultorder->num_rows > 0) {
-									while ($row = $resultorder->fetch_assoc()) {
-										$order_menu_json = $row['orderMenu'];
+						if ($resultorder->num_rows > 0) {
+							while ($row = $resultorder->fetch_assoc()) {
+								$order_menu_json = $row['orderMenu'];
 
-										// แปลง JSON เป็น associative array
-										$order_menu_data = json_decode($order_menu_json, true);
+								// แปลง JSON เป็น associative array
+								$order_menu_data = json_decode($order_menu_json, true);
 
-										if (isset($order_menu_data['order'])) {
-											$order_items = $order_menu_data['order'];
+								if (isset($order_menu_data['order'])) {
+									$order_items = $order_menu_data['order'];
 
-											foreach ($order_items as $order_item) {
-												$menu_id =  $order_item['menuId'];
-												$name = "SELECT * FROM Menu WHERE menuID = $menu_id;";
-												$resultmenu = mysqli_query($conn->getDatabase(), $name);
-												if (mysqli_num_rows($resultmenu) > 0) {
-													while ($row = mysqli_fetch_assoc($resultmenu)) {
-														echo '<div class="row">
+									foreach ($order_items as $order_item) {
+										$menu_id =  $order_item['menuId'];
+										$name = "SELECT * FROM Menu WHERE menuID = $menu_id;";
+										$resultmenu = mysqli_query($conn->getDatabase(), $name);
+										if (mysqli_num_rows($resultmenu) > 0) {
+											while ($row = mysqli_fetch_assoc($resultmenu)) {
+												echo '<div class="row">
 																<div class="col-9">' . $row['menu_name'] . '</div>
 																<div class="col-3">' . $order_item['menuCount'] . '</div>
 															</div><br>';
-													}
-												}
 											}
 										}
 									}
 								}
+							}
+						}
 						echo '
 									</div>
 									<h6 class="card-subtitle mb-2 text-muted">
 										<div class="bottom-text">
-											<button class="button-24" role="button">กำลังทำ</button>
-											<button class="button-24G" role="button">เสร็จสิ้น</button>
+											<button class="button-24" onclick="updateOrderStatus('.$orderid.',\'doing\')" role="button">กำลังทำ</button>
+											<button class="button-24G" onclick="updateOrderStatus('.$orderid.',\'finish\')" role="button">เสร็จสิ้น</button>
 									</div>
 								</div>
 							</div>
@@ -144,12 +172,12 @@ $conn = new database();
 				}
 			}
 
-	?>
-			</div>
+			?>
 		</div>
-		<script src="scripts.js"></script>
-		<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
-		<script src="./script.js"></script>
+	</div>
+	<script src="scripts.js"></script>
+	<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
+	<script src="./script.js"></script>
 </body>
 
 </html>
---
+++ Kitchen_status/scripts.js
@@ -0,0 +1,33 @@
+function updateOrderStatus(id, status) {
+    // ส่งค่า table_status และ tableID ไปยัง PHP script ด้วย Fetch API
+    let formData = new URLSearchParams();
+    formData.append('order_id', id);
+    formData.append('order_status', status);
+
+    fetch('./index.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: formData.toString()
+    })
+    .then(response => {
+        if (response.ok) {
+            Swal.fire({
+                icon: "success",
+                title: "เปลี่ยนสถานะรายการอาหารเสร็จสิ้น",
+                showConfirmButton: false,
+                timer: 3500
+              });
+            setTimeout(function() {
+                location.reload();
+            }, 1000);
+            return response.text();
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .catch(error => {
+        alert('There was a problem with the fetch operation: ' + error.message);
+    });
+    
+}
