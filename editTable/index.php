67e30358d0fc1997832f248f132386f59f993947
SupakornThongaerd <supakorn642@gmail.com>
2024-03-02 18:54:03 +0700

    up

--- editTable/index.php
+++ editTable/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="png" sizes="96x96" href="../image_logo/logo.png" />
+    <link rel="icon" type="png" sizes="96x96" href="../image_logo/logo.png"/>
     <link href="styles.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
