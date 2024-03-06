5cfc16076ea694ee103589f500858aafabe0fc49
DraftManan <draft347@gmail.com>
2024-03-06 16:54:36 +0700

    up

--- Check_status/index.php
+++ Check_status/index.php
@@ -115,13 +115,20 @@ $order_status = "";
                     <div class="container">
                         <div class="centeritem">
                         <div class="texthead">
-                            <div style="height: 10px"></div>
-                                <h2><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-bag-check" viewBox="0 0 16 20">
-                                <path fill-rule="evenodd" d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-                              </svg>ออเดอร์ที่สั่ง</h2>
-                              <h5 style="color: black; display: inline; height: auto;">ข้อมูล ณ เวลา <p id="refreshTime" style="vertical-align: middle; display: inline; font-size: 18px;"></p></h5>
-                              <div style="height: 2px"></div>
+                            <div style="height: 2px"></div>
+                                <div>
+                                    <h2 class="orderhead">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-bag-check" viewBox="0 0 16 20">
+                                            <path fill-rule="evenodd" d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+                                            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                                        </svg> ออเดอร์ที่สั่ง
+                                    </h2>
+
+                                    <div class="detailhead">ข้อมูล ณ เวลา 
+                                        <p id="refreshTime"></p>
+                                    </div>
+                                </div>
+                              <div style="height: 4px"></div>
                                 </div>
                             </div>
                         </div>
