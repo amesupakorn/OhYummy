8d75c151288bb8a7a3dd7d8c45c0c3a409551565
SupakornThongaerd <supakorn642@gmail.com>
2024-03-07 18:10:49 +0700

    up

--- reserve/reserve.php
+++ reserve/reserve.php
@@ -115,17 +115,19 @@
 
     <?php
           if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if(isset($_POST['name']) && isset($_POST['tel']) && isset($_POST['seat']) && isset($_POST['email']) && isset($_POST['date'])){
+            // if(isset($_POST['name']) && isset($_POST['tel']) && isset($_POST['seat']) && isset($_POST['email']) && isset($_POST['date'])){
               
-              $name = $_POST['name'];
-              $tel = $_POST['tel'];
-              $email = $_POST['email'];
-              $seat = $_POST['seat'];
-              $date = $_POST['date'];
-              echo 'dd';
-              $sql = "INSERT INTO Reserve(cust_Name, cust_Tel, email, Reserve_time) VALUES ('$name', '$tel', '$email', $date)";
-              mysqli_query($conn->getDatabase(), $sql);
-            }
+            //   $name = $_POST['name'];
+            //   $tel = $_POST['tel'];
+            //   $email = $_POST['email'];
+            //   $seat = $_POST['seat'];
+            //   $date = $_POST['date'];
+            //   echo 'dd';
+            //   $sql = "INSERT INTO Reserve(cust_Name, cust_Tel, email, Reserve_time) VALUES ('$name', '$tel', '$email', $date)";
+            //   mysqli_query($conn->getDatabase(), $sql);
+            // }
+
+           
           
           }
     ?>
@@ -135,23 +137,23 @@
       <div class="form-row">
           <div class="form-group col-12 col-md-6">
               <label for="input">จำนวนที่นั่ง</label>
-                  <select  class="custom-select" id="normal-select-1" placeholder-text="2 คน" id="inlineFormCustomSelect">
-                    <option value="2" class="select-dropdown__list-item">1 คน</option>
-                    <option value="2" class="select-dropdown__list-item">2 คน</option>
-                    <option value="4" class="select-dropdown__list-item">3 คน</option>
-                    <option value="4" class="select-dropdown__list-item">4 คน</option>
-                    <option value="6" class="select-dropdown__list-item">5 คน</option>
-                    <option value="6" class="select-dropdown__list-item">6 คน</option>
+                  <select  class="custom-select" placeholder-text="เลือกโต๊ะสำหรับคุณ" id="seat" onchange="report()">
+                    <option value="1" class="select-dropdown__list-item">โต๊ะที่ 1 สำหรับ 1 - 2 คน</option>
+                    <option value="2" class="select-dropdown__list-item">โต๊ะที่ 2 สำหรับ 1 - 2 คน</option>
+                    <option value="3" class="select-dropdown__list-item">โต๊ะที่ 3 สำหรับ 3 - 4 คน</option>
+                    <option value="4" class="select-dropdown__list-item">โต๊ะที่ 4 สำหรับ 3 - 4 คน</option>
+                    <option value="5" class="select-dropdown__list-item">โต๊ะที่ 5 สำหรับ 5 - 6 คน</option>
+                    <option value="6" class="select-dropdown__list-item">โต๊ะที่ 6 สำหรับ 5 - 6 คน</option>
                   </select>
                                 
             </div>
             <div class="form-group col-md-6">
-              <label for="input">&nbsp;&nbsp;&nbsp;&nbsp;วันที่</label>
+              <label for="input">&nbsp;วันที่</label>
               <div x-data="app()" x-init="[initDate(), getNoOfDays()]" x-cloak>
                 <div class="mb-5 w-100 ">
                   <div class="relative">
-                    <input type="hidden" name="date" x-ref="date" :value="datepickerValue" />
-                    <input type="text" x-on:click="showDatepicker = !showDatepicker" x-model="datepickerValue" x-on:keydown.escape="showDatepicker = false" style="height: 42px" class=" bg-white w-full pl-4 leading-none rounded-lg shadow-sm focus:outline-none text-gray-600 font-medium focus:ring focus:ring-blue-600 focus:ring-opacity-50" placeholder="Select date" readonly />
+                    <input type="hidden"  name="date" x-ref="date" :value="datepickerValue" />
+                    <input type="text" id="date"  x-on:click="showDatepicker = !showDatepicker" x-model="datepickerValue" x-on:keydown.escape="showDatepicker = false" style="height: 42px" class=" bg-white w-full pl-4 leading-none rounded-lg shadow-sm focus:outline-none text-gray-600 font-medium focus:ring focus:ring-blue-600 focus:ring-opacity-50" placeholder="Select date" readonly />
 
                     <div class="absolute top-0 right-0 px-3 py-2">
                       <svg class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -201,9 +203,9 @@
                         <template x-for="(date, dateIndex) in no_of_days" :key="dateIndex">
                           <div style="width: 14.28%" class="px-1 mb-1">
                             <div @click="getDateValue(date)" x-text="date" class="cursor-pointer text-center text-sm leading-none rounded-full leading-loose transition ease-in-out duration-100" :class="{
-                                'bg-indigo-200': isToday(date) == true, 
-                                'text-gray-600 hover:bg-indigo-200': isToday(date) == false && isSelectedDate(date) == false,
-                                'bg-indigo-500 text-white hover:bg-opacity-75': isSelectedDate(date) == true 
+                                'bg-red-200': isToday(date) == true, 
+                                'text-gray-600 hover:bg-red-200': isToday(date) == false && isSelectedDate(date) == false,
+                                'bg-red-500 text-white hover:bg-opacity-75': isSelectedDate(date) == true 
                               }"></div>
                           </div>
                         </template>
@@ -216,33 +218,143 @@
             </div>
           
 
+            <button style="width: 200px; text-align:" id="confirmButton" class="btn btn-danger" onclick="report()" >เช็ค</button><br><br>
 
     <hr style="width: 100%; ">
     <div style="height: 30px"></div> 
     
     <h5>เลือกช่วงเวลาที่มี:</h5>
 
-    <div class="button-container">
-  <button class="button" id="myButton1">11.00 น.</button>
-  <button class="button" id="myButton2">12.00 น.</button>
-  <button class="button" id="myButton3">13.00 น.</button>
-  <button class="button" id="myButton4">14.00 น.</button>
-  <button class="button" id="myButton5">15.00 น.</button>
-  <button class="button" id="myButton6">16.00 น.</button>
-</div>
 
-<div class="button-container">
-  <button class="button" id="myButton7">17.00 น.</button>
-  <button class="button" id="myButton8">18.00 น.</button>
-  <button class="button" id="myButton9">19.00 น.</button>
-  <button class="button" id="myButton10">20.00 น.</button>
-  <button class="button" id="myButton11">21.00 น.</button>
-  <button class="button" id="myButton12">22.00 น.</button>
-</div>
+  <?php
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if(isset($_POST['reserveDate'])){
 
-<div class="button-container">
+        $date = $_POST['reserveDate'];
+        $table = $_POST['table'];
 
-</div>
+        $formatDate = date('Y-m-d', strtotime($date));
+        
+        $sql = "SELECT * FROM Reserve r WHERE r.reserve_day = '$formatDate' AND tableid = $table";
+        $result = mysqli_query($conn->getDatabase(), $sql);
+					
+        if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+            echo  '<div class="button-container">';
+              // For 11.00
+              if($row['reserve_time'] == '11:00:00'){
+                echo '<button class="buttondis" id="myButton1" disabled>11.00 น.</button>';
+              } else {
+                echo '<button class="button" id="myButton1">11.00 น.</button>';
+              }
+
+              // For 12.00
+              if($row['reserve_time'] == '12:00:00'){
+                echo '<button class="buttondis" id="myButton2" disabled>12.00 น.</button>';
+              } else {
+                echo '<button class="button" id="myButton2">12.00 น.</button>';
+              }
+
+              // For 13.00
+              if($row['reserve_time'] == '13:00:00'){
+                echo '<button class="buttondis" id="myButton3" disabled>13.00 น.</button>';
+              } else {
+                echo '<button class="button" id="myButton3">13.00 น.</button>';
+              }
+
+              // For 14.00
+              if($row['reserve_time'] == '14:00:00'){
+                echo '<button class="buttondis" id="myButton4" disabled>14.00 น.</button>';
+              } else {
+                echo '<button class="button" id="myButton4">14.00 น.</button>';
+              }
+
+              // For 15.00
+              if($row['reserve_time'] == '15:00:00'){
+                echo '<button class="buttondis" id="myButton5" disabled>15.00 น.</button>';
+              } else {
+                echo '<button class="button" id="myButton5">15.00 น.</button>';
+              }
+
+              // For 16.00
+              if($row['reserve_time'] == '16:00:00'){
+                echo '<button class="buttondis" id="myButton6" disabled>16.00 น.</button>';
+              } else {
+                echo '<button class="button" id="myButton6">16.00 น.</button>';
+              }
+            echo '</div>';
+
+            echo '<div class="button-container">';
+              // For 17.00
+                if($row['reserve_time'] == '17:00:00'){
+                  echo '<button class="buttondis" id="myButton7" disabled>17.00 น.</button>';
+                } else {
+                  echo '<button class="button" id="myButton7">17.00 น.</button>';
+                }
+
+                // For 18.00
+                if($row['reserve_time'] == '18:00:00'){
+                  echo '<button class="buttondis" id="myButton8" disabled>18.00 น.</button>';
+                } else {
+                  echo '<button class="button" id="myButton8">18.00 น.</button>';
+                }
+
+                // For 19.00
+                if($row['reserve_time'] == '19:00:00'){
+                  echo '<button class="buttondis" id="myButton9" disabled>19.00 น.</button>';
+                } else {
+                  echo '<button class="button" id="myButton9">19.00 น.</button>';
+                }
+
+                // For 20.00
+                if($row['reserve_time'] == '20:00:00'){
+                  echo '<button class="buttondis" id="myButton10" disabled>20.00 น.</button>';
+                } else {
+                  echo '<button class="button" id="myButton10">20.00 น.</button>';
+                }
+
+                // For 21.00
+                if($row['reserve_time'] == '21:00:00'){
+                  echo '<button class="buttondis" id="myButton11" disabled>21.00 น.</button>';
+                } else {
+                  echo '<button class="button" id="myButton11">21.00 น.</button>';
+                }
+
+                // For 22.00
+                if($row['reserve_time'] == '22:00:00'){
+                  echo '<button class="buttondis" id="myButton12" disabled>22.00 น.</button>';
+                } else {
+                  echo '<button class="button" id="myButton12">22.00 น.</button>';
+                }
+              echo '</div>';
+          }
+        }
+        else {
+          // ถ้าไม่มีการจองในวันและเวลานั้น แสดงปุ่มปกติ
+          echo 'งง';
+      }
+      }
+    }
+        else{
+          echo '   <div class="button-container">
+          <button class="button" id="myButton1">11.00 น.</button>
+          <button class="button" id="myButton2">12.00 น.</button>
+          <button class="button" id="myButton3">13.00 น.</button>
+          <button class="button" id="myButton4">14.00 น.</button>
+          <button class="button" id="myButton5">15.00 น.</button>
+          <button class="button" id="myButton6">16.00 น.</button>
+        </div>
+        
+        <div class="button-container">
+          <button class="button" id="myButton7">17.00 น.</button>
+          <button class="button" id="myButton8">18.00 น.</button>
+          <button class="button" id="myButton9">19.00 น.</button>
+          <button class="button" id="myButton10">20.00 น.</button>
+          <button class="button" id="myButton11">21.00 น.</button>
+          <button class="button" id="myButton12">22.00 น.</button>
+        </div>';
+        }
+  ?>
 
 <div style="height: 60px;"></div>
       <div class="center-container"><br>
--- reserve/script.js
+++ reserve/script.js
@@ -87,6 +87,7 @@ function createSelect() {
       var element = document.getElementById(id);
       element.value = valueToSelect;
       element.setAttribute('selected', 'selected');
+
     }
     var buttonSelect = document.getElementsByClassName('select-dropdown__button');
     for (var i = 0, len = buttonSelect.length; i < len; i++) {
@@ -96,3 +97,189 @@ function createSelect() {
 			}, false);
 		}
 }
+
+
+
+
+// ----------------datepicker--------------------
+
+const MONTH_NAMES = [
+    "มกราคม",
+    "กุมภาพันธ์",
+    "มีนาคม",
+    "เมษายน",
+    "พฤษภาคม",
+    "มิถุนายน",
+    "กรกฎาคม",
+    "สิงหาคม",
+    "กันยายน",
+    "ตุลาคม",
+    "พฤศจิกายน",
+    "ธันวาคม",
+  ];
+  const MONTH_SHORT_NAMES = [
+    "ม.ค.",
+    "ก.พ.",
+    "มี.ค.",
+    "เม.ย.",
+    "พ.ค.",
+    "มิ.ย.",
+    "ก.ค.",
+    "ส.ค.",
+    "ก.ย.",
+    "ต.ค.",
+    "พ.ย.",
+    "ธ.ค.",
+  ];
+  const DAYS = ["อาทิตย์", "จันทร์", "อังคาร", "พุธ", "พฤหัส", "ศุกร์", "เสาร์"];
+
+  function app() {
+    return {
+      showDatepicker: false,
+      datepickerValue: "",
+      dateFormat: "DD-MM-YYYY",
+      month: "",
+      year: "",
+      no_of_days: [],
+      blankdays: [],
+      initDate() {
+        let today;
+        if (this.selectedDate) {
+          today = new Date(Date.parse(this.selectedDate));
+        } else {
+          today = new Date();
+        }
+        this.month = today.getMonth();
+        this.year = today.getFullYear();
+        this.datepickerValue = this.formatDateForDisplay(
+          today
+        );
+      },
+      formatDateForDisplay(date) {
+        let formattedDay = DAYS[date.getDay()];
+        let formattedDate = ("0" + date.getDate()).slice(
+          -2
+        ); // appends 0 (zero) in single digit date
+        let formattedMonth = MONTH_NAMES[date.getMonth()];
+        let formattedMonthShortName =
+          MONTH_SHORT_NAMES[date.getMonth()];
+        let formattedMonthInNumber = (
+          "0" +
+          (parseInt(date.getMonth()) + 1)
+        ).slice(-2);
+        let formattedYear = date.getFullYear();
+        if (this.dateFormat === "DD-MM-YYYY") {
+          return `${formattedDate}-${formattedMonthInNumber}-${formattedYear}`; // 02-04-2021
+        }
+        if (this.dateFormat === "YYYY-MM-DD") {
+          
+          return `${formattedYear}-${formattedMonthInNumber}-${formattedDate}`; // 2021-04-02
+        }
+        if (this.dateFormat === "D d M, Y") {
+          return `${formattedDay} ${formattedDate} ${formattedMonthShortName} ${formattedYear}`; // Tue 02 Mar 2021
+        }
+        return `${formattedDay} ${formattedDate} ${formattedMonth} ${formattedYear}`;
+      },
+      isSelectedDate(date) {
+        const d = new Date(this.year, this.month, date);
+        return this.datepickerValue ===
+          this.formatDateForDisplay(d) ?
+          true :
+          false;
+      },
+      isToday(date) {
+        const today = new Date();
+        const d = new Date(this.year, this.month, date);
+        return today.toDateString() === d.toDateString() ?
+          true :
+          false;
+      },
+      getDateValue(date) {
+        let selectedDate = new Date(
+          this.year,
+          this.month,
+          date
+        );
+        this.datepickerValue = this.formatDateForDisplay(
+          selectedDate
+        );
+        checkValue(this.datepickerValue);
+        // this.$refs.date.value = selectedDate.getFullYear() + "-" + ('0' + formattedMonthInNumber).slice(-2) + "-" + ('0' + selectedDate.getDate()).slice(-2);
+        this.isSelectedDate(date);
+        this.showDatepicker = false;
+      },
+      getNoOfDays() {
+        let daysInMonth = new Date(
+          this.year,
+          this.month + 1,
+          0
+        ).getDate();
+        // find where to start calendar day of week
+        let dayOfWeek = new Date(
+          this.year,
+          this.month
+        ).getDay();
+        let blankdaysArray = [];
+        for (var i = 1; i <= dayOfWeek; i++) {
+          blankdaysArray.push(i);
+        }
+        let daysArray = [];
+        for (var i = 1; i <= daysInMonth; i++) {
+          daysArray.push(i);
+        }
+        this.blankdays = blankdaysArray;
+        this.no_of_days = daysArray;
+      },
+    };
+  }
+
+
+  var buttons = document.querySelectorAll(".button");
+
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+      // ถ้าปุ่มถูกเลือกอยู่แล้วให้เอา class "selected" ออก
+      var selectedButton = document.querySelector(".button.selected");
+      if (selectedButton) {
+        selectedButton.classList.remove("selected");
+      }
+      // เพิ่ม class "selected" ให้กับปุ่มที่ถูกคลิก
+      this.classList.add("selected");
+    });
+  });
+
+
+
+
+
+function checkValue(date){
+    var seat= document.getElementById('seat').value
+    var dateReserve = date
+
+    let formData = new URLSearchParams();
+    formData.append('reserveDate', dateReserve);
+    formData.append('table', seat);
+
+	fetch('./reserve.php', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: formData.toString()	
+	})
+	.then(response => {
+		// setTimeout(function() {
+		// 	location.reload();
+		// }, 1000);
+		return response.text();
+	})
+	.catch(error => {
+		alert('There was a problem with the fetch operation: ' + error.message);
+	});
+}
+
+
+
+
+
+
